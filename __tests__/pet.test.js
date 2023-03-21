7f9636b367862b29adf08d59de50cb5b513f801c
Oliver Owen <oliverorangee@gmail.com>
2023-03-21 17:16:26 +0000

    created a growUp function and working test

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,4 +4,24 @@ describe('construtor', () => {
     it('returns an object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
     }); 
+
+    it('sets the name property', () => {
+        const pet = new Pet('Fido');
+
+        expect(pet.name).toEqual('Fido');
+    });
+
+    it('sets the starting age to 0', () => {
+        const pet = new Pet('Fido');
+
+        expect(pet.age).toEqual(0);
+    });
+
+    it('increments the age by 1', () => {
+        const pet = new Pet('Fido');
+
+        pet.growUp();
+
+        expect(pet.age).toEqual(1);
+    });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,11 @@
-function Pet() {}
+ageStart = 0;
+
+function Pet(name) {
+    this.name = name;
+    this.age = ageStart
+    this.growUp = function() {
+        this.age += 1;
+    };
+}
 
 module.exports = Pet;
